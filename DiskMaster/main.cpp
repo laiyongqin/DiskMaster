ae1b91415d52b89ad2b9b69652014661c6ab4385
ssidko <troyvova@gmail.com>
2015-06-19 15:22:08 +0300

    First commit

---
+++ DiskMaster/main.cpp
@@ -0,0 +1,17 @@
+// DiskMaster.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <windows.h>
+#include "ftd2xx.h"
+#include "FTChipID.h"
+
+#pragma comment(lib, "ftd2xx.lib")
+#pragma comment(lib, "FTChipID.lib")
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
