f3b581f462dd7e5588a3115bc60ce2d6c3a3ed11
ssidko <troyvova@gmail.com>
2015-06-22 18:53:28 +0300

    experimental

---
+++ DiskMaster/ForTest.h
@@ -0,0 +1,104 @@
+#ifndef _FOR_TEST_H
+#define _FOR_TEST_H
+
+#include <windows.h>
+#include <dbt.h>
+
+#include <exception>
+#include <system_error>
+
+#define HARDWARE_MONITOR_CLASS_NAME					"Hardware PnP Monitor CLASS"
+#define HARDWARE_MONITOR_WND_NAME					"Hardware PnP Monitor WND"
+
+class HardwareMonitor
+{
+private:
+	HWND wnd;
+	static LRESULT CALLBACK StaticWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		PDEV_BROADCAST_HDR header = (PDEV_BROADCAST_HDR)lParam;
+		DEV_BROADCAST_PORT *port_info = nullptr;
+		TCHAR *name;
+		if (uMsg == WM_DEVICECHANGE) {
+			
+			switch (wParam) {
+			case DBT_DEVICEARRIVAL:
+				if (header->dbch_devicetype == DBT_DEVTYP_PORT) {
+					port_info = (PDEV_BROADCAST_PORT)(lParam);
+					name = port_info->dbcp_name;
+						
+				}
+
+				break;
+			case DBT_DEVICEREMOVECOMPLETE:
+				if (header->dbch_devicetype == DBT_DEVTYP_PORT)
+
+				break;
+			}
+			return TRUE;
+		}
+		else return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+
+	void RegisterWindowClass(void)
+	{
+		HINSTANCE hinst = ::GetModuleHandle(NULL);
+		WNDCLASSEXA wce;
+		memset(&wce, 0x00, sizeof(WNDCLASSEX));
+
+		wce.cbSize = sizeof(wce);
+		wce.lpfnWndProc = HardwareMonitor::StaticWindowProc;
+		wce.hInstance = hinst;
+		wce.lpszClassName = HARDWARE_MONITOR_CLASS_NAME;
+
+		if (!::RegisterClassExA(&wce)) {
+			std::error_code error(::GetLastError(), std::system_category());
+			if (ERROR_CLASS_ALREADY_EXISTS != error.value())
+				throw std::system_error(error, "HardwareMonitor::RegisterWindowClass()");
+		}
+	}
+
+	void UnregisterWindowClass(void)
+	{
+		DestroyWindow();
+		::UnregisterClassA(HARDWARE_MONITOR_CLASS_NAME, ::GetModuleHandle(NULL));
+	}
+
+	void CraeteWindow(void)
+	{
+		HINSTANCE hinst = ::GetModuleHandle(NULL);
+		wnd = ::CreateWindowA(HARDWARE_MONITOR_CLASS_NAME, HARDWARE_MONITOR_WND_NAME, 0,
+			CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hinst, NULL);
+		if (!wnd) {
+			std::error_code error(::GetLastError(), std::system_category());
+			throw std::system_error(error, "HardwareMonitor::CraeteWindow()");
+		} 
+	}
+
+	void DestroyWindow(void)
+	{
+		if (wnd) {
+			::DestroyWindow(wnd);
+			wnd = NULL;
+		}
+	}
+
+	void Initialise(void)
+	{
+		RegisterWindowClass();
+		CraeteWindow();
+	}
+
+public:
+	HardwareMonitor(void) : wnd(NULL)
+	{
+		Initialise();
+	}
+
+	~HardwareMonitor(void)
+	{
+		UnregisterWindowClass();
+	}
+};
+
+#endif // _FOR_TEST_H
--- DiskMaster/main.cpp
+++ DiskMaster/main.cpp
@@ -7,11 +7,32 @@
 #include "ftd2xx.h"
 #include "FTChipID.h"
 
+#include "ForTest.h"
+
 #pragma comment(lib, "ftd2xx.lib")
 #pragma comment(lib, "FTChipID.lib")
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	HardwareMonitor hw_monitor;
+
+	////////////////////////////////////////////////////////
+	// Message loop
+	//
+	MSG msg;
+	BOOL ret = FALSE;
+	memset(&msg, 0x00, sizeof(MSG));
+	while (ret = ::GetMessage(&msg, NULL, 0, 0)) {
+		if (ret == -1) {
+			return ::GetLastError();
+		}
+		else {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
 	return 0;
 }
 
